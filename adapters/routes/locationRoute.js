6bf2f362544758ff3d8a190d74e8b324acfb1686
Sinan Aydın <87941390+AydinSinan@users.noreply.github.com>
2021-12-30 01:14:30 +0300

    Update locationRoute.js

--- adapters/routes/locationRoute.js
+++ adapters/routes/locationRoute.js
@@ -4,6 +4,6 @@ const locationController=require("../controllers/locationController")
 const router = express.Router()
 
 router.route("/gps_list").get(locationController.getGPS);
-router.route("/gps_add").post(locationController.addGPS);
+router.route("/gps_append").post(locationController.appendGPS);
 
 module.exports = router
