1d73f1682a75bd587af50931392040bbc4040808
Sinan Aydın <87941390+AydinSinan@users.noreply.github.com>
2021-12-30 01:20:25 +0300

    Update vehicleRoute.js

--- adapters/routes/vehicleRoute.js
+++ adapters/routes/vehicleRoute.js
@@ -4,7 +4,7 @@ const vehicleController=require("../controllers/vehicleController")
 const router = express.Router()
 
 router.route("/vehicle_list").get(vehicleController.getVehicles);
-router.route("/vehicle_add").post(vehicleController.addVehicle);
+router.route("/vehicle_add").post(vehicleController.appendVehicle);
 router.route("/vehicle_update").put(vehicleController.updateVehicle);
 router.route("/vehicle_delete").delete(vehicleController.deleteVehicle);
 
